be8c375109a01a672075604610438fefb22b696b
TariqTarbuck <t.tarbuck@hotmail.com>
2017-09-26 17:26:03 +1000

    Finished excercise One

---
+++ DebuggingExercises/BookArray.java
@@ -0,0 +1,25 @@
+public class BookArray {
+    public static void main(String[] args) {
+
+        Book aBook[] = new Book[10];
+        int count;
+
+        aBook[0] = new Fiction("The best book in the world");
+        aBook[1] = new Fiction("The best book in the world vol 21");
+        aBook[2] = new Fiction("The best book in the world vol 22");
+        aBook[3] = new Fiction("The best book in the world vol 45");
+        aBook[4] = new Fiction("The best book in the world vol 45.2");
+        aBook[5] = new Fiction("The best book in the world vol 89");
+        aBook[6] = new Fiction("The best book in the world vol 90");
+        aBook[7] = new Fiction("The best book in the world vol 45");
+        aBook[8] = new Fiction("The best book in the world vol 34");
+        aBook[9] = new Fiction("The best book in the world vol 10");
+
+        for (count = 0; count < aBook.length; ++count){
+            System.out.println("Book: "+aBook[count].getBookTitle()
+                    +"Costs: $"+aBook[count].getBookPrice());
+        }
+
+
+    }
+}
--- DebuggingExercises/UseBook.java
+++ DebuggingExercises/UseBook.java
@@ -2,4 +2,18 @@
  * Created by jc320680 on 26/09/17.
  */
 public class UseBook {
+    public static void main(String[] args) {
+        Fiction novel = new Fiction("The best book in the world");
+
+        NonFiction notANovel = new NonFiction("KFC Menu");
+
+        System.out.println("The fiction novel: " +novel.getBookTitle());
+        System.out.println("A fiction novel costs: " + novel.getBookPrice());
+
+
+        System.out.println("The non fiction novel: " +notANovel.getBookTitle());
+        System.out.println("A non fiction novel costs: " +notANovel.getBookPrice());
+
+    }
+
 }
